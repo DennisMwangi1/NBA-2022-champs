eb63963c7722ca98b086bf8ac6966853b8a781cd
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-23 11:10:15 +0300

    updated the images in the images folder in assets .Got the ids of the respective html elements in the script.js.

--- src/script.js
+++ src/script.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded',()=>{
     let firstName = document.getElementById("firstName")
     let lastName = document.getElementById("lastName")
+    let playerHeight = document.getElementById('heightOfPlayer')
+    let playerWeight = document.getElementById("weightOfPlayer")
+    let playerBirthDate = document.getElementById("born")
+    let playerFrom = document.getElementById('from')
+    let playerDebut = document.getElementById('debut')
+    let playerPrevious = document.getElementById('previously')
+    let playerNumber = document.getElementById('Num')
+    let playerPosition = document.getElementById('position')
+    let playerImage = document.getElementById('imageOfPlayer')
     let curry = document.getElementById('stephenCurry')
     let klay = document.getElementById('klayThompson')
+
+
+
     curry.addEventListener('click',(e)=>{
         e.preventDefault()
         firstName.textContent = 'Stephen'
